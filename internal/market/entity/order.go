3e003e7eba0f921532f6c5495f5b1f5a0c952873
TiagoMontes <tiagosantos6303@gmail.com>
2023-06-19 20:23:42 -0500

    <feat>: Creating order entity and NewOrder function

---
+++ internal/market/entity/order.go
@@ -0,0 +1,34 @@
+package entity
+
+type OrderType string
+
+const (
+    BUY OrderType = "BUY"
+    SELL = "SELL"
+)
+
+type Order struct {
+	ID string
+	Investor *Investor //esse é nosso investor de investor.go
+	Asset *Asset //esse é nosso Asset de asset.go
+	Shares int
+	PendingShares int
+	Price float64
+	OrderType OrderType
+	Status string
+	Transactions []*Transaction
+}
+
+func NewOrder(orderID string, investor *Investor, asset *Asset, shares int, price float64, orderType OrderType) *Order{
+	return &Order{
+		ID: orderID,
+		Investor: investor,
+		Asset: asset,
+		Shares: shares,
+		PendingShares: shares,
+		Price: price,
+		OrderType: orderType,
+		Status: "OPEN", //TODA ORDEM AO CRIADA, SERÁ SEMPRE ABERTA, DEPOIS DISSO, CLOSED
+		Transactions: []*Transaction{},
+	}
+}
